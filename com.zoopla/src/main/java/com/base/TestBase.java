c5f9d2913f190741914ec89f8742192d4203b0c9
maimuddin <mo.maimuddin@gmail.com>
2019-05-20 16:00:36 +0530

    Added project

---
+++ com.zoopla/src/main/java/com/base/TestBase.java
@@ -0,0 +1,46 @@
+package com.base;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+	public static WebDriver driver;
+	public static Properties prop;
+	private static int pagetimeout;
+	private static int impli_timeout;
+	
+	public TestBase(){
+		prop = new Properties();
+		try{
+			FileInputStream fl = new FileInputStream(System.getProperty(""));
+			prop.load(fl);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initilization(){
+		String browser = prop.getProperty("browser");
+		pagetimeout = Integer.parseInt(prop.getProperty("pagetimeout"));
+		impli_timeout = Integer.parseInt(prop.getProperty("implicite_timeout"));
+		if(browser.equalsIgnoreCase("Chrome")){
+			System.setProperty("", "");
+			driver = new ChromeDriver();
+		}
+		else if (browser.equalsIgnoreCase("Firefox")){
+			System.setProperty("", "");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(pagetimeout, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(impli_timeout, TimeUnit.SECONDS);
+	}
+}
