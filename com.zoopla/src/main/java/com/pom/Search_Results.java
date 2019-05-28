6a837cb51e0112dc05e5b8163114fde0890a68f8
maimuddin <mo.maimuddin@gmail.com>
2019-05-28 15:14:20 +0530

    adding new result class

---
+++ com.zoopla/src/main/java/com/pom/Search_Results.java
@@ -0,0 +1,29 @@
+package com.pom;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class Search_Results {
+	
+	Search_Results(){	
+	}
+	
+	By getAmount = By.xpath(".//a[contains(@class, 'listing-results-price text-price')]");
+	
+	By getAddress = By.xpath(".//a[contains(@class, 'listing-results-address')]");
+	
+	
+	@FindBy(how = How.XPATH, using = "//li[contains(@id, 'listing_')]")
+	public List<WebElement> results;
+	
+	public WebElement getAmountElement(int n){
+			return results.get(n-1).findElement(getAmount);	
+	}
+	public WebElement getAddressElement(int n){
+		return results.get(n-1).findElement(getAddress);	
+}
+}
--- com.zoopla/src/test/java/zoopla/com/zoopla/Search_Location.java
+++ com.zoopla/src/test/java/zoopla/com/zoopla/Search_Location.java
@@ -5,12 +5,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.base.TestBase;
+import com.pom.Search_Results;
 import com.pom.Search_page;
 
 public class Search_Location extends TestBase {
 	
 	Search_page search;
 	Search_Location s;
+	Search_Results result;
 	
 	public Search_Location() {
 		super();
@@ -21,12 +23,17 @@ public class Search_Location extends TestBase {
 		s = new Search_Location();
 		initilization();
 		search = PageFactory.initElements(driver, Search_page.class);
+		result = PageFactory.initElements(driver, Search_Results.class);
+		
 	}
 
 	@Test
 	public void search(){
+		search.acceptCookie();
 		search.search_box.sendKeys("London");
 		search.search_button.click();
-		search.acceptCookie();
+		System.out.println(result.getAddressElement(5).getText());
+		System.out.println(result.getAmountElement(5).getText());
+		
 	}
 }
